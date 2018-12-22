4290d059e3d0da3d17b54467171310443a26470d
JackyrDC <30815697+JackyrDC@users.noreply.github.com>
2018-12-21 18:59:07 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>
+            Auto Lote
+        </title>
+        <meta name="charset" content="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap\bootstrap.min.css">
+        <link rel="stylesheet" href="fontawesome\css\all.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="bootstrap/jquery-3.3.1.min.js"></script>
+        <script src="bootstrap/popper.min.js"></script>
+        <script src="bootstrap/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid" id="navbar">
+            <div class="row">
+            <h1 id="title" class="col-3 ">Auto Lote</h1>
+            <div class="col-7">
+            </div>
+            <button class="btn btn-outline-dark col-2" style="display:inline-block;">Ingresar</button>
+            </div> 
+        </div>   
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/Ford GT40.jpg" alt="Ford GT40" style="height:700px;">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>Ford GT40</h5>
+    <p>Auto limpio y con clase</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/koenigsegg-agera.jpg" alt="Koenigsegg Agera" style="height:700px;">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>Koenigsegg Agera</h5>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/Ferrari575M maranello.jpg" alt="Ferrari 575 Maranello" style="height:700px;">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>Ferrari 575 Maranello</h5>
+    </div>
+    </div>
+  </div>
+<div class="container-fluid mt-5">
+  <div class="row">
+  <div class="col-1 col-sm-0"></div>
+    <div class="card col-3">
+      <div class="card-header-pills bg-info">
+        <h4 class="bars text-center ">
+         Los mejores autos al mejor precio
+        </h4>
+      </div>
+      <div class="card-body">
+        <div class="row mb-1">
+          <img class="card-img w-25 h-25 border-dark col-4" src="img\Logo_bmw.jpg" alt="No hay nada">
+          <img class="card-img w-25 h-25 border-dark col-4" src="img\logo_Citroën.png" alt="No hay nada">
+          <img class="card-img w-25 h-25 border-dark col-4" src="img\logo_Lambo.png" alt="No hay nada">
+      </div>
+      <div class="row">
+          <img class="card-img w-25 h-25 border-dark col-4" src="img\logo_koenigsegg.jpg" alt="No hay nada">
+          <img class="card-img w-25 h-25 border-dark col-4" src="img\Maserati_logo.jpg" alt="No hay nada">
+          <img class="card-img w-25 h-25 border-dark col-4" src="img\Mercedes_logo.png" alt="No hay nada">
+      </div>
+      <h4 class="card-text m-2 ml-4 text-justify" style="align:center;">  
+          Con las marcas más reconocidas a nivel mundial.
+      </h4>
+
+      </div>
+      <div class="card-footer bg-info mb-1">
+
+    </div>
+  </div>
+</div>
+    </body>
+</html>
